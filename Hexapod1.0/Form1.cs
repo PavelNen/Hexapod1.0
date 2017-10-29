38b907c25c78d9a424493fab65f7eb47e1091414
PavelNen <nps-11@mail.ru>
2017-10-29 20:58:59 +0300

    First movement

--- Hexapod1.0/Form1.cs
+++ Hexapod1.0/Form1.cs
@@ -16,5 +16,45 @@ namespace Hexapod1._0
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.IO.Ports.SerialPort sport = new System.IO.Ports.SerialPort(textBox1.Text,
+                                                                              115200,
+                                                                              System.IO.Ports.Parity.None,
+                                                                              8,
+                                                                              System.IO.Ports.StopBits.One);
+
+            try {
+                sport.Open();
+                sport.Write("#0P1000#3P1000\r");
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.ToString());
+            }
+
+            sport.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.IO.Ports.SerialPort sport = new System.IO.Ports.SerialPort(textBox1.Text,
+                                                                              115200,
+                                                                              System.IO.Ports.Parity.None,
+                                                                              8,
+                                                                              System.IO.Ports.StopBits.One);
+
+            try
+            {
+                sport.Open();
+                sport.Write("#0P700T2000#3P700T2000\r");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            sport.Close();
+        }
     }
 }
